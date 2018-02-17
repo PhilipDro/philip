d62baf4b1ef7457164bea73fe10da68dfa26ad0e
Philip Drozd <me@philipdrozd.com>
2018-02-17 03:20:25 +0100

    adding project html files, editing projects with images

--- gulpfile.js
+++ gulpfile.js
@@ -3,43 +3,44 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
-// var clean = require('gulp-clean');
+var clean = require('gulp-clean');
 
-gulp.task('sass', function () {
-  return gulp.src('./src/sass/**/*.sass')
+gulp.task('sass', ['copy'], function () {
+  return gulp.src('./src/sass/main.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./target/css'));
 });
 
-gulp.task('coffee', function() {
+gulp.task('coffee', ['sass'], function() {
   gulp.src('./src/coffee/*.coffee')
     .pipe(coffee({bare: true}))
     .pipe(gulp.dest('./target/js/'));
 });
 
-// gulp.task('clean', function () {
-//     return gulp.src('target/js', {read: false})
-//         .pipe(clean());
-// });
+gulp.task('clean', function () {
+    return gulp.src('target/', {read: false})
+        .pipe(clean());
+});
 
-gulp.task('concat', function() {
+gulp.task('concat', ['coffee'], function() {
   return gulp.src('./target/js/*.js')
     .pipe(concat('scripts.js'))
     .pipe(gulp.dest('./target/js/'));
 });
 
-gulp.task('copy', function() {
+gulp.task('copy', ['clean'], function() {
   gulp.src(['src/**.*'])
    .pipe(dest('target/'))
    .pipe(gulp.dest('./'));
    gulp.src(['src/images/**.*'])
     .pipe(dest('target/images/'))
     .pipe(gulp.dest('./'));
+   gulp.src(['src/projects/**.*'])
+    .pipe(dest('target/projects/'))
+    .pipe(gulp.dest('./'));
 });
 
 
-gulp.task('default', function() {
-  gulp.watch('./src/**/*.*', ['copy', 'sass',
-  //'coffee',
-   'concat']);
+gulp.task('default', ['sass', 'coffee', 'concat'], function() {
+  gulp.watch('./src/**/*.*', ['copy', 'concat']);
 });
